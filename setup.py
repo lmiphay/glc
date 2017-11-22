4884a3272172f54995a92b1167c3bdace4c1f50c
Paul Healy <lmiphay@gmail.com>
2017-11-22 21:48:34 +0000

    select glc module

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(name='glc',
       author='Paul Healy',
       url='https://github.com/lmiphay/glc',
       packages=[
-          'bun'
+          'glc'
       ],
       install_requires=['invoke', 'pyyaml'],
       entry_points={
         'console_scripts': ['glc = glc.main:program.run']
       },
       data_files=[
-            ('/etc/glc', [
+            ('/etc/bun', [
                   'etc/glc.yaml'
             ]),
             ('share/oam/site', [
